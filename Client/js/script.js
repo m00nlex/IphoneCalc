e775311ae8147e639432d091df3329d9040b7d61
moonlex <m00n-lex@yandex.ru>
2022-11-06 23:39:45 +0500

    nums comma and action buttons

--- Client/js/script.js
+++ Client/js/script.js
@@ -1,1 +1,21 @@
-console.log("Script.js is started now. . . ")
+// let curValue = 0;
+// let memValue = 0;
+//
+// function update(value){
+//     curValue = value;
+//     document.getElementById('input').innerHTML = curValue;
+// }
+//
+// document.getElementById('buttonHolder').onclick((event) =>{
+//     let target = event.target;
+//
+//     if(target.classList.contains('grayB')){
+//         if(target.id === 'button18'){
+//             if(curValue.toString() === '0'){
+//                 update('0,')
+//             } else {
+//                 update(curValue.toString() + ',')
+//             }
+//         }
+//     }
+// })
